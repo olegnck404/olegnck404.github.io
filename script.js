c95a3e643bcd950fe566a594002892a3988ddb74
Oleh Liubchenko <doroleijn@gmail.com>
2023-08-24 15:02:02 +0200

    add

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ switchMode.onclick = function() {
         theme.href = "dark.css"
     }
 }
+
+window.addEventListener('load', function() {
+    var currentDate = new Date();
+
+    var birthDate = new Date("2006-06-03");
+
+    var age = currentDate.getFullYear() - birthDate.getFullYear();
+
+    var birthDateThisYear = new Date(currentDate.getFullYear(), birthDate.getMonth(), birthDate.getDate());
+
+    if (currentDate < birthDateThisYear) {
+        age--;
+    }
+
+    document.getElementById("age").textContent = age;
+});
